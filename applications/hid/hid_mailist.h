2d7b02e8ea834592222b4e1acca5662538afffac
Roberto Alanis <roberto.alanis@continental-corporation.com>
2018-08-28 14:28:02 -0500

    hid process

---
+++ applications/hid/hid_mailist.h
@@ -0,0 +1,12 @@
+#ifndef HID_MAILIST_H_
+#define HID_MAILIST_H_
+
+#define HID_INT_MAILIST(X) \
+X(HID_INT_BLINK_TOUT_MID,  "hid_blink_tout") \
+X(HID_INT_START_BLINK_MID, "hid_start_blink") \
+X(HID_INT_STOP_BLINK_MID,  "hid_stop_blink") \
+X(HID_INT_BUZZ_SUCESS_MID, "hid_buzz_success") \
+X(HID_INT_BUZZ_ERROR_MID,  "hid_buzz_error") \
+X(HID_INT_BUZZ_INIT_MID,   "hid_buzz_init") \
+
+#endif /*HID_MAILIST_H_*/
